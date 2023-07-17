3b175c319a61d2602f67e1105091414510bad2e4
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-17 21:38:01 +0300

    Add :  Cookie settings for MVC Client1

--- IdentityServer.Client1/Program.cs
+++ IdentityServer.Client1/Program.cs
@@ -3,6 +3,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//..
+// üyelik sistemlerini (cookie mekanizmalarýný ayýrmak için þemalar kullanýlýr, ör. bayii ve kullanýcý için mesela. Ayrý þemalar gerekecekti.
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "mySiteCookies";  // Aþaðýdakilerle eþleþmeli
+    options.DefaultChallengeScheme = "oidc"; // Aþaðýdakilerle eþleþmeli // open Id connect
+}).AddCookie("mySiteCookies").AddOpenIdConnect("oidc", opts =>
+{
+    opts.SignInScheme = "mySiteCookies";
+    opts.Authority = "https://localhost:7112"; // Auth Server yetkili
+    opts.ClientId = "Client1-Mvc";
+    opts.ClientSecret = "secret";
+    opts.ResponseType = "code id_token"; // Notes.Return Type
+});
+
+//..
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
