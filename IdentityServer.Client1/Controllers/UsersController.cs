4b336e81414aa7a78bccf3e1f485562f996e5f8f
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-18 23:16:26 +0300

    Add : Add usage of refresh token for Client1-Mvc via GetRefreshToken() and refresh the token

--- IdentityServer.Client1/Controllers/UsersController.cs
+++ IdentityServer.Client1/Controllers/UsersController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Globalization;
 using System.Security.Claims;
 
 namespace IdentityServer.Client1.Controllers
@@ -9,6 +13,13 @@ namespace IdentityServer.Client1.Controllers
     [Authorize]
     public class UsersController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             //User.Claims
@@ -32,5 +43,58 @@ namespace IdentityServer.Client1.Controllers
             await HttpContext.SignOutAsync("oidc"); // Identity atafında (AuthServer)  Config içinde PostLogoutRedirectUris=new List<string>{"\"https://localhost:7086/signout-callback-oidc"} belirttiğimiz adrese yönlendirir böylece
 
         }
+
+
+        public async Task<IActionResult> GetRefreshToken()
+        {
+            // refresh tokenın cookie içinde  bilgileri var, yenisi alınınca onlar da güncellenmeli RefreshTokenUsage, RefreshTokenExpiration, AbsoluteRefreshTokenLifetime vb gibi yani tekrar signin gerek
+
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            HttpClient httpClient = new HttpClient();
+
+
+            var discoveryEndpoint = await httpClient.GetDiscoveryDocumentAsync("https://localhost:7112");
+
+            if (discoveryEndpoint.IsError)
+            {
+                //discoveryEndpoint.Error...
+            }
+
+            RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest();
+            refreshTokenRequest.ClientId = _configuration["Client1Mvc:ClientId"];// "Client1-Mvc";
+            refreshTokenRequest.ClientSecret = _configuration["Client1Mvc:ClientSecret"]; //"secret";
+            refreshTokenRequest.RefreshToken = refreshToken;
+            refreshTokenRequest.Address = discoveryEndpoint.TokenEndpoint;
+
+            var tokenResponse = await httpClient.RequestRefreshTokenAsync(refreshTokenRequest);
+
+            if (tokenResponse.IsError)
+            {
+                // hata varsa yönlendir ....
+            }
+
+            var tokens = new List<AuthenticationToken>()
+            {
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.AccessToken , Value = tokenResponse.AccessToken },
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.IdToken , Value = tokenResponse.IdentityToken },
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.RefreshToken , Value = tokenResponse.RefreshToken },
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.ExpiresIn , Value = DateTime.UtcNow.AddSeconds(tokenResponse.ExpiresIn).ToString("O", CultureInfo.InvariantCulture) },
+            };
+
+            var authenticationResult = await HttpContext.AuthenticateAsync();
+            var properties = authenticationResult.Properties; // Authentication propertyeri al
+
+            properties.StoreTokens(tokens); // token güncelleme
+
+            await HttpContext.SignInAsync("mySiteCookies", authenticationResult.Principal, properties);
+
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
     }
 }
