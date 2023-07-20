66ec4ad2bc26d9c052cbe1127741ab99870603eb
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-20 16:27:42 +0300

    Add : ICustomUserRepository

---
+++ IdentityServer.AuthServer/Services/ICustomUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using IdentityServer.AuthServer.Models;
+
+namespace IdentityServer.AuthServer.Services
+{
+    public interface ICustomUserRepository
+    {
+        Task<bool> Validate(string email, string password);
+        Task<CustomUser> FindById(int id);
+        Task<CustomUser> FindByEmail(string emai);
+
+    }
+}
